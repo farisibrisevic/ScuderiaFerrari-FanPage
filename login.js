7a0ebdf6c17829da9e9e9b9de0fd34062351c02a
Faris Ibrišević <faris.ibrisevic.22@size.ba>
2024-04-26 14:41:40 +0200

    Add files via upload

--- login.js
+++ login.js
@@ -5,22 +5,22 @@ const path = require('path');
 
 const app = express();
 
-// Middleware to parse JSON and URL-encoded form data
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static files from the 'public' directory
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Load users from JSON file
+
 let users = JSON.parse(fs.readFileSync('users.json', 'utf8')).users;
 
-// Route to serve login form
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
-// Route to handle login form submission
+
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
 
@@ -34,27 +34,22 @@ app.post('/login', (req, res) => {
   }
 });
 
-// Route to handle registration form submission
+
 app.post('/register', (req, res) => {
   const { regUsername, regPassword } = req.body;
 
-  // Check if the username already exists
+  
   const existingUser = users.find(u => u.username === regUsername);
 
   if (existingUser) {
     res.redirect('/?registerError=Username already exists. Please choose a different username, or use existing credentials.');
   } else {
-    // Add new user to the users array
     users.push({ username: regUsername, password: regPassword });
-
-    // Update the users.json file with the new user data
     fs.writeFileSync('users.json', JSON.stringify({ users }, null, 2));
-
     res.send('Registration successful!');
   }
 });
 
-// Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
