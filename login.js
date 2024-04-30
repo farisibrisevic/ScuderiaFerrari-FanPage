e5e019326cb1021d273ec23bdecaea34e8a110e3
Faris Ibrišević <faris.ibrisevic.22@size.ba>
2024-04-30 23:45:07 +0200

    Add files via upload

--- login.js
+++ login.js
@@ -5,32 +5,42 @@ const path = require('path');
 
 const app = express();
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const directoryPath = __dirname;
+const usersFilePath = path.join(directoryPath, 'users.json');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(directoryPath));
 
+// Initialize users array
+let users = [];
 
-let users = JSON.parse(fs.readFileSync('users.json', 'utf8')).users;
-
+// Read users data from JSON file
+try {
+  const userData = fs.readFileSync(usersFilePath, 'utf8');
+  users = JSON.parse(userData).users;
+} catch (err) {
+  console.error('Error reading users file:', err);
+}
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+  const { loginError, registerError } = req.query;
+  res.sendFile(path.join(directoryPath, 'login.html'));
 });
-
 
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
 
-  // Find user in the JSON file
+  // Find user in the users array
   const user = users.find(u => u.username === username && u.password === password);
 
   if (user) {
-    res.send('Login successful!');
+    // Redirect to index.html upon successful login
+    res.redirect('/index.html');
   } else {
-    res.redirect('/?loginError=Invalid username or password.');
+    // Render login page with login error message
+    res.sendFile(path.join(directoryPath, 'login.html'), { loginError: 'Invalid username or password.' });
   }
 });
 
@@ -38,17 +48,23 @@ app.post('/login', (req, res) => {
 app.post('/register', (req, res) => {
   const { regUsername, regPassword } = req.body;
 
-  
+  // Check if the username already exists
   const existingUser = users.find(u => u.username === regUsername);
 
   if (existingUser) {
-    res.redirect('/?registerError=Username already exists. Please choose a different username, or use existing credentials.');
+    // Render login page with registration error message
+    return res.sendFile(path.join(directoryPath, 'login.html'), { registerError: 'Username already exists. Please choose a different username.' });
   } else {
+    // Add new user to the users array
     users.push({ username: regUsername, password: regPassword });
-    fs.writeFileSync('users.json', JSON.stringify({ users }, null, 2));
+
+    // Update the users.json file with the new user data
+    fs.writeFileSync(usersFilePath, JSON.stringify({ users }, null, 2));
+
     res.send('Registration successful!');
   }
 });
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
