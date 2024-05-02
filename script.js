0345f0eba6b4b6709a2e314f4dbd7e4eb5cd301a
Faris Ibrišević <faris.ibrisevic.22@size.ba>
2024-05-02 16:19:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -103,3 +103,12 @@ function stickyFunction(id) {
       header.classList.remove("sticky");
   }
 }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
