d70e66c77ac7bc63549706102693cbf7c04002f1
Faris Ibrišević <faris.ibrisevic.22@size.ba>
2024-05-27 00:04:43 +0200

    Male UI promjene

--- vrijeme.js
+++ vrijeme.js
@@ -7,14 +7,26 @@ document.addEventListener('DOMContentLoaded', function () {
             timeZone: 'Europe/Rome',
             hour: '2-digit',
             minute: '2-digit',
-            //second: '2-digit',
             year: 'numeric',
             month: '2-digit',
             day: '2-digit'
         };
-        const formatter = new Intl.DateTimeFormat([], options);
-        document.getElementById('maranello-time').innerText = formatter.format(now);
+        const formatter = new Intl.DateTimeFormat('en-GB', options); 
+        const parts = formatter.formatToParts(now);
+    
+        let day, month, year, hour, minute;
+        parts.forEach(part => {
+            if (part.type === 'month') month = part.value;
+            if (part.type === 'day') day = part.value;
+            if (part.type === 'year') year = part.value;
+            if (part.type === 'hour') hour = part.value;
+            if (part.type === 'minute') minute = part.value;
+        });
+    
+        const formattedDate = `${day}/${month}/${year} ${hour}:${minute}`;
+        document.getElementById('maranello-time').innerText = formattedDate;
     }
+    
 
     async function fetchRaceSchedule() {
         try {
